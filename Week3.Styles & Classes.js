8eafd1f17f6af1586e24ab4f6e807d8c337c1f7b
matthewhodgin <95104960+matthewhodgin@users.noreply.github.com>
2022-02-13 22:31:11 -0700

    Add files via upload

---
+++ Week3.Styles & Classes.js
@@ -0,0 +1,19 @@
+
+var h2Element = document.createElement('h2') 
+h2Element.innerText = 'Hello Again, World'
+document.body.append(h2Element);
+
+var h2List = document.getElementById("h2")
+for (var i = 0; i < h2Element.length; i++) {
+    h2List.innerHTML += 5
+
+
+var heading = document.getElementById('heading');
+heading.style.color = 'cornflowerblue';
+heading.fontsize(20);
+heading.fontweight(lighter);
+heading.fontfamily(sans-serif);
+
+heading.classList.add('new-class')
+console.log(heading.classList)
+}
